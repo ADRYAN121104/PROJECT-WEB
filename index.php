1a730bd576927cc474c480ee4f6ee3219dd2eaef
ADRYAN121104 <madryanryan80@gamail.com>
2026-04-14 21:00:48 +0800

    update v1

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "adryan";
+$umur = 17;
+$tinggi = 175.9;
+$hobi = ["membaca", "bermain game", "berolahraga"];
+ echo "nama saya $nama, umur saya $umur tahun, tinggi saya $tinggi cm, dan hobi saya adalah ". implode(",", $hobi);
+?>
