b3299071b6ff37dd61e86b90a6ead4ee58c67bb0
ADRYAN121104 <madryanryan80@gmail.com>
2026-05-12 08:40:17 +0800

    update harian

--- materi4.php
+++ materi4.php
@@ -1,11 +1,10 @@
 <form action="" method="post">
-    ID : <input type="text" name="id"><br>
     nama : <input type="text" name="nama"><br>
     password : <input type="password" name="password"><br> 
     email : <input type="text" name="email"><br>
     alamat : <input type="text" name="alamat"><br>
     <input type="submit" value="kirim data" name="kirim"><br>
-</form> 
+</form>
 
 <?php
 $koneksi = mysqli_connect("localhost", "root", "", "db_wisata");
@@ -44,7 +43,9 @@ if (isset($_POST['kirim'])) {
         echo "<td>" . $row['password'] . "</td>";
         echo "<td>" . $row['email'] . "</td>";
         echo "<td>" . $row['alamat'] . "</td>";
-        echo "<td><a href='edit.php?id=" . $row['id'] . "'>Edit</a> | <a href='delete.php?id=" . $row['id'] . "'>Delete</a></td>";
+       echo "<td>
+<a href='?aksi=edit&id=" . $row['id'] . "'>Edit</a> | <a href='?aksi=hapus&id=" . $row['id'] . "' onclick=\"return confirm('Yakin hapus?')\">Delete</a>
+</td>";
         echo "</tr>";
         
      }
@@ -78,3 +79,44 @@ if (isset($_POST['kirim'])) {
             }
     ?>
     </table>
+
+
+
+    // proses edit
+    <?php
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "SELECT * FROM user WHERE id = $id";
+        $result = mysqli_query($koneksi, $query);
+        $row = mysqli_fetch_assoc($result);
+
+        $username = $row['nama'];
+        $password = $row['password'];
+        $email = $row['email'];
+        $alamat = $row['alamat'];
+
+        ?>
+<form action="" method="post">
+    <input type="text" name="nama" value="<?= $nama ?>"><br>
+    <input type="password" name="password" value="<?= $password ?>"><br>
+    <input type="text" name="email" value="<?= $email ?>"><br>
+    <input type="text" name="alamat" value="<?= $alamat ?>"><br>
+    <input type="submit" value="update data" name="update"><br>
+</form>
+<?php
+
+        if (isset($_POST['update'])) {
+            $nama = $_POST['nama'];
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+            $alamat = $_POST['alamat'];
+
+            $query = "UPDATE user SET nama='$nama', password='$password', email='$email', alamat='$alamat' WHERE id=$id";
+            if (mysqli_query($koneksi, $query)) {
+                echo "berhasil update data";
+            } else {
+                echo "Error: " . mysqli_error($koneksi);
+            }
+        }
+       
+    }
